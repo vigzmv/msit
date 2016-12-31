6afb66b87c97adc508a4fc3164f9ed8c12b2439b
M Vignesh <vigzmv@outlook.com>
2016-12-31 12:51:30 +0530

    Update facultySorter.js

--- facultySorter.js
+++ facultySorter.js
@@ -1,6 +1,5 @@
 ["#robotics", "#quizzes"]
     .forEach(item => { document.querySelector(`${item} > div > table > tbody`)
     .innerHTML = Array.from(document.querySelectorAll(`${item} > div > table > tbody > tr`))
-    .sort((a, b) => a.querySelector('strong').innerHTML.split(" ")[1] > b.querySelector('strong').innerHTML
-    .split(" ")[1] ? 1 : -1).map(s => s.outerHTML).join(" ")
-});
+    .sort((a, b) => a.querySelector('strong').innerHTML.split(" ")[1] > b.querySelector('strong').innerHTML.split(" ")[1] ? 1 : -1)
+    .map(s => s.outerHTML).join(" ")});
